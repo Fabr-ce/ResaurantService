bc4f7e13aea87d519dbffd9c7dbaf2d778b3f44e
fabrice.egger@tbwil.ch <fabrice.egger@tbwil.ch>
2023-04-23 10:40:16 +0200

    prepared for deploy

--- backend/server.ts
+++ backend/server.ts
@@ -20,7 +20,7 @@ app.get('*', (req, res) => {
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "http://" + location.host,
+        origin: "http://localhost:3000",
         methods: ["GET", "POST"]
     }
 });
